115db12b6868b8f455c702695a569356df8fe98f
Matt <matthew_d_barrett@hotmail.com>
2022-02-18 19:36:12 +1100

    Time: 50 ms (36.18%), Space: 25.7 MB (23.12%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,22 +3,13 @@ public:
     void rotate(vector<int>& nums, int k) {
         int numsSize = nums.size();
         vector<int> temp(numsSize);
-        
-//         for ( int i = nums.size()-k; i <  nums.size(); i++ )
-//             temp.push_back(nums[i]); 
-        
-//         for ( int i = 0; i < nums.size()-k; i++)
-//             temp.push_back(nums[i]);
-            
-//         nums = temp;
-        
-        
+
         for ( int i = 0; i < numsSize; i++ ) { 
-            int newIndex = i + k + 1;
-            while ( newIndex > numsSize )
-                newIndex -= numsSize;
+            int newIndex = i + k ;
+            
+            newIndex = newIndex - (newIndex/numsSize) * numsSize;
                 
-            temp[newIndex-1] = nums[i];
+            temp[newIndex] = nums[i];
         }
         nums = temp;
     }
