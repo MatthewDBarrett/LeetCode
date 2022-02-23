3bbc65715e7641d74f0a5ba7333121b378fd7604
Matt <matthew_d_barrett@hotmail.com>
2022-02-23 14:33:50 +1100

    Time: 15 ms (47.92%), Space: 11.7 MB (6.50%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        int m = mat.size();     //rows
-        int n = 0;              //columns
-        
-        
         vector<int> tempMat;
         
-        for ( int i = 0; i < m; i++ )
+        for ( int i = 0; i < mat.size(); i++ )
             for ( int j = 0; j < mat[i].size(); j++ ) 
                 tempMat.push_back( mat[i][j] );
         
-        int t = tempMat.size();
         vector<vector<int>> transMat(r, vector<int>(c,0));
 
-        if ( t != r * c )
+        if ( tempMat.size() != r * c )
             return mat;
-        
-//         for ( int i = 0; i < m; i++ ) {         //rows
-//             n = mat[i].size();
-//             for ( int j = 0; j < n; j++ ) {     //columns
                 
-//             }
-//         }
-        
         int count = 0;
         
         for ( int i = 0; i < r; i++ ) {
